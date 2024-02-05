ca74a8a42cc87cf518154fb8b5491a4ccf2c5765
pmellaaho <pmellaaho@gmail.com>
2024-02-05 20:04:15 +0200

    Solution for day 05 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,39 +1,30 @@
-import java.lang.Exception
-import java.util.InputMismatchException
-import kotlin.math.cbrt
-import kotlin.system.measureTimeMillis
-
 data class Block(val src: Long, val dst: Long, val length: Long)
 data class SeedsMap(val from: String, val to: String, val blocks: List<Block>)
 
 fun main() {
 
-    /**
-     * In this example, the calibration values of these four lines are 12, 38, 15, and 77.
-     * Adding these together produces 142.
-     */
     fun part1(input: List<String>): Long {
-
         val seeds = input.first().let { str ->
             Regex("\\d+").findAll(str).map { it.value.toLong() }
-        }.toList()
+        } //.toList()
+        "Nbr of seeds: ${seeds.count()}".println()
 
         val mapList =
-                input.drop(2).fold(mutableListOf(mutableListOf<String>())) { acc, string ->
-                    if (string.isBlank()) {
-                        acc.add(mutableListOf<String>())
-                    } else {
-                        acc.last().add(string)
-                    }
-                    acc
-                }.map {
-                    val (from, _, to) = it.first().split("-", " ")
-                    val blocks = it.drop(1).map {
-                        val (dst, src, length) = it.split(" ").map { it.toLong() }
-                        Block(src, dst, length)
-                    }
-                    SeedsMap(from, to, blocks)
+            input.drop(2).fold(mutableListOf(mutableListOf<String>())) { acc, string ->
+                if (string.isBlank()) {
+                    acc.add(mutableListOf<String>())
+                } else {
+                    acc.last().add(string)
                 }
+                acc
+            }.map {
+                val (from, _, to) = it.first().split("-", " ")
+                val blocks = it.drop(1).map {
+                    val (dst, src, length) = it.split(" ").map { it.toLong() }
+                    Block(src, dst, length)
+                }
+                SeedsMap(from, to, blocks)
+            }
 
         return seeds.minOf { seed ->
             mapList.fold<SeedsMap, Long>(seed) { acc, map ->
@@ -44,41 +35,74 @@ fun main() {
     }
 
     /**
-     * In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76.
-     * Adding these together produces 281.
+     *
      */
-    fun part2(input: List<String>): Int {
-        var res = 0
-        return res
+    fun part2(input: List<String>): Long {
+        val seeds = input.first().let { str ->
+            Regex("\\d+").findAll(str).map { it.value.toLong() }
+        }.toList().chunked(2).map {
+            it.first()..it.first() + it.last()
+        }
+        seeds.println()
+
+        val mapList =
+            input.drop(2).fold(mutableListOf(mutableListOf<String>())) { acc, string ->
+                if (string.isBlank()) {
+                    acc.add(mutableListOf<String>())
+                } else {
+                    acc.last().add(string)
+                }
+                acc
+            }.map {
+                val (from, _, to) = it.first().split("-", " ")
+                val blocks = it.drop(1).map {
+                    val (dst, src, length) = it.split(" ").map { it.toLong() }
+                    // dst and src must be switched when going from location to seed
+                    Block(dst, src, length)
+                }
+                SeedsMap(to, from, blocks)
+            }.reversed()
+
+        var location = 0L // 6400L
+        var seed: Long = 0L
+        while (seeds.none {
+                it.contains(seed)
+            }
+        ) {
+            "no seed found for location: $location".println()
+            location += 1
+            seed = mapList.findSeedForLocation(location)
+        }
+
+        "Seed: $seed found for location: $location".println()
+        return location
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-//    check(part1(testInput) == 35L)
+    check(part1(testInput) == 35L)
+    check(part2(testInput) == 46L)
 
     val input = readInput("Day05")
-    measureTimeMillis {
-        part1(input).println()
-    }.println()
 //    check(part1(input) == 910845529L)
+//    check(part2(input) == 77435348L)
 //    part2(input).println()
 }
+
+fun List<SeedsMap>.findSeedForLocation(location: Long) =
+    fold(location) { acc, map ->
+        val mapped = acc.mapToValue(map)
+        mapped
+    }
+
 
 private fun Long.mapToValue(input: SeedsMap): Long {
     input.blocks.forEach { block ->
         val sourceRange = block.src..block.src + block.length
 
-        // Why is this so much less (10x) performant than the code below??
-//        if (sourceRange.contains(this)) {
-//            return block.dst + sourceRange.indexOf(this)
-//        }
-
         if (sourceRange.contains(this)) {
-            for ((index, value) in sourceRange.withIndex()) {
-                if (value == this) {
-                    return block.dst + index
-                }
-            }
+            return block.dst + (this - block.src)
+//            return block.dst + sourceRange.indexOf(this)
         }
     }
     return this
